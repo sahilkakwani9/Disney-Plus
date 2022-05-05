ea8d091922cffcfe1c4f4cf6eac1c4c3750d287c
sahilkakwani9 <sahkakwani@gmail.com>
2022-05-06 01:43:06 +0530

    navbar banaya

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,29 @@ function Navbar() {
                     <img src="./images/home-icon.svg" alt="" />
                     <span>HOME</span>
                 </a>
+                <a>
+                    <img src="./images/search-icon.svg" alt="" />
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src="./images/watchlist-icon.svg" alt="" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src="./images/original-icon.svg" alt="" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src="./images/movie-icon.svg" alt="" />
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src="./images/series-icon.svg" alt="" />
+                    <span>SERIES</span>
+                </a>
+
             </NavMenu>
+            <UsrImg src='./images/sahil-headshot.JPG'/>
         </Nav></>
     
   )
@@ -25,6 +47,7 @@ const Nav = styled.nav`
     display:flex;
     align-items:center;
     padding: 0 36px;
+    justify-content: space-around;
     `
 
 const Logo = styled.img`
@@ -32,13 +55,46 @@ const Logo = styled.img`
 
 const NavMenu = styled.div`
         display:flex;
+        {/* flex:1; */}
+        margin-left:25px;
+        align-items: center;
 
         a{
             display:flex;
             align-items:center;
             padding: 0 12px;
+            cursor:pointer;
             img{
                 height: 20px;
             }
+            span{
+                font-size: 13px;
+                letter-spacing: 1.42px;
+                position: relative;
+
+                &:after {
+                    content ="";
+                    height: 2px;
+                    width:3px;
+                    background: white;
+                    opacity:1;
+                    position: absolute;
+                }
+            }
+
+            &:after {
+                content ="sahil";
+                    height: 2px;
+                    width:3px;
+                    background: white;
+                    opacity:1;
+            }
         }
 `
+
+const UsrImg = styled.img`
+        width: 48px;
+        height: 48px;
+        border-radius: 50%;
+        cursor:pointer;
+    `
